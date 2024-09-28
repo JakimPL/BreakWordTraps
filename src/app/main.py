0c0b8141a292afebfec0d477c5b22eab7d1734b8
JakimPL <jakimpl@gmail.com>
2024-09-28 14:01:43 +0200

    First commit

---
+++ src/app/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Update this to the port your frontend is running on
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "query": q}
+
+@app.post("/items/")
+def create_item():
+    return {"message": f"Item created successfully!"}
