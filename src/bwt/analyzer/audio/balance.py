c9a86f1b107095a02e73c811cfe602acab7181f1
JakimPL <jakimpl@gmail.com>
2024-09-29 07:40:14 +0200

    Minor updates

--- src/bwt/analyzer/audio/balance.py
+++
@@ -1,9 +0,0 @@
-from typing import Any, Dict
-import os
-
-from bwt.analyzer.audio.audio_analyzer import AudioAnalyzer
-
-
-class BalanceAnalyzer(AudioAnalyzer):
-    def __call__(self, audio_path: os.PathLike) -> Dict[str, Any]:
-        pass
