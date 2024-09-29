5811bf430dbd27ebbb9df850a80f96dec3446849
JakimPL <jakimpl@gmail.com>
2024-09-29 08:21:34 +0200

    Added: video processing

--- backend/main.py
+++ backend/main.py
@@ -1,44 +1,20 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, File, UploadFile
+from pathlib import Path
+from bwt.pipeline.pipeline import Pipeline
 
 app = FastAPI()
 
-"""app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Update this to the port your frontend is running on
-    allow_credentials=True,
-    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allow all headers
-)
 
+@app.post("/process_video/")
+async def process_video(file: UploadFile = File(...)):
+    tmp_dir = Path("/tmp/video_transcription/")
+    tmp_dir.mkdir(parents=True, exist_ok=True)
+    file_path = tmp_dir / file.filename
 
-def download_from_bucket(bucket_name, source_blob_name, destination_file_name):
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(source_blob_name)
-    blob.download_to_filename(destination_file_name)
-    print(f"Downloaded {source_blob_name} from bucket {bucket_name} to {destination_file_name}")
+    with open(file_path, "wb") as buffer:
+        buffer.write(await file.read())
 
-"""
-"""@app.get("/run_pipeline/")
-def read_root():
-    bucket_name = "gs://breakthewordstraps-bucket-hackyeah/sample_video.mov"
-    source_blob_name = "HY_2024_film_20.mp4"  # Ścieżka do pliku w buckecie
-    local_file_name = "/tmp/HY_2024_film_20.mp4"  # Tymczasowe miejsce zapisania pliku
-    
-    # Pobierz plik z bucketa
-    download_from_bucket(bucket_name, source_blob_name, local_file_name)
-    
-    # Uruchom pipeline z pobranym plikiem
-    output = Pipeline(local_file_name)
-    
-    return {"message": "Pipeline uruchomiony", "output": output}
-"""
+    pipeline = Pipeline()
+    output = pipeline(file_path)
 
-
-
-
-
-@app.post("/items/")
-def create_item(data: dict):
-    return {"message": f"Item created successfully!"}
+    return output
